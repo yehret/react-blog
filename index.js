8243b096137d7ad0f2cde8f88668ff8a14d7851f
Serhii <sirozhka.v@gmail.com>
2023-04-23 14:22:51 +0200

    creating jwt token

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/auth/login', (req, res) => {
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullname: 'Daniel',
+    },
+    'hornydog',
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(4444, (err) => {
