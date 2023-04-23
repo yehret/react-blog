fc03e97a1269c1ee75887b7618826f186f644a99
Serhii <sirozhka.v@gmail.com>
2023-04-23 14:53:50 +0200

    add custom messages for validators

--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,8 @@
 import { body } from 'express-validator';
 
 export const registerValidation = [
-  body('email').isEmail(),
-  body('password').isLength({ min: 5 }),
-  body('fullName').isLength({ min: 3 }),
-  body('avatarUrl').optional().isURL(),
+  body('email', 'Wrong email format').isEmail(),
+  body('password', 'Password should be a minimum of 5 symbols').isLength({ min: 5 }),
+  body('fullName', 'Enter your name').isLength({ min: 3 }),
+  body('avatarUrl', 'Wrong image URL').optional().isURL(),
 ];
