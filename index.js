9119e832ef368e5d4fefebe594ce2db0ba365701
Serhii <sirozhka.v@gmail.com>
2023-04-23 14:33:05 +0200

    Connecting to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(
+    'mongodb+srv://serge:R6XXezMn8BeFy5lz@cluster0.eww0rwe.mongodb.net/?retryWrites=true&w=majority',
+  )
+  .then(() => {
+    console.log('DB is connected');
+  })
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 
