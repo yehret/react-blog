edbeaa795aeada7e05f312b7d17688d426816430
Serhii <sirozhka.v@gmail.com>
2023-04-23 14:50:56 +0200

    express validators

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+
+import { registerValidation } from './validations/auth.js';
 
 mongoose
   .connect(
@@ -15,7 +18,15 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', (req, res) => {});
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) return res.status(400).json(errors.array());
+
+  res.json({
+    success: true,
+  });
+});
 
 app.listen(4444, (err) => {
   if (err) {
